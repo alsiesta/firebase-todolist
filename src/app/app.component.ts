3dda6c83ba478076c9d90d01208e223316b8a055
alsiesta <as@daslizenzatelier.de>
2023-03-14 16:48:19 +0100

    installed firestore

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
 import { Component } from '@angular/core';
+import { Firestore, collectionData, collection } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+  
 export class AppComponent {
-  title = 'firebase-todolist';
+  todos$: Observable<any>;
+  // das Dollarzeichen kann man auch weglassen. Best-Practise ist jedoch, dass man es nutzt, um anzuseigen, dass diese Variable sich stetig updated! Any nehmen wir als Datentyp, wenn wir ein JSON einbinden
+  constructor(firestore: Firestore) { //hier importieren wir die Lib Firestore als Objekt und weisen sie der Var firestore zu
+    const coll = collection(firestore, 'todos');//aus unserem firestore wollen die gesamte collection haben
+    this.todos$ = collectionData(coll); //und aus der collection wollen wir alle daten haben
+
+    // bei jedem updated wird diese subscribe fn aufgerufen
+    this.todos$.subscribe((newTodos) => {
+      console.log('Neue Todos sind: ', newTodos)
+    })
+  }
 }
---
+++ src/environments/environment.ts
@@ -0,0 +1,9 @@
+export const environment = {
+  firebase: {
+    projectId: 'fir-todolist-58848',
+    appId: '1:411337934328:web:e3983770d67a7720ff020d',
+    storageBucket: 'fir-todolist-58848.appspot.com',
+    apiKey: 'AIzaSyCgduXFeWqBDCc2acltQHvWmrdLyWFhH7E',
+    authDomain: 'fir-todolist-58848.firebaseapp.com',
+    messagingSenderId: '411337934328',
+  },};
